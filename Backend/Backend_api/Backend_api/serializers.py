bbdf56bc63726f15f45758f1bdf71bc02dc49a3e
Rails Abam <abamsunday5@gmail.com>
2022-02-18 23:16:54 +0100

    commiting

---
+++ Backend/Backend_api/Backend_api/serializers.py
@@ -0,0 +1,17 @@
+from email.headerregistry import Group
+from importlib.metadata import files
+from django.contrib.auth.models import Users
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Users
+        fields = ['Username', 'name', 'Password']
+        
+        
+        
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = []
